3ea052a53fee49d6dc1527f0c27a74ce885fadcf
anavictoriaea <avea@aluno.ifnmg.edu.br>
2022-07-21 08:11:57 -0300

    4 files modified

--- atividades/atv2.js
+++ atividades/atv2.js
@@ -1,13 +1,10 @@
-function dia() {
-  let hoje = new Date;
-  let diasDaSemana = new Array("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado");
-  let saida = document.getElementById("texto");
-  let diaHoje = hoje.getDay();
-  let ontem = diaHoje - 1;
-  let amanha = diaHoje + 1;
-  let data = hoje.getDate() + "/" + hoje.getMonth() + 1 + "/" + hoje.getFullYear();
-  saida.innerHTML = <p>Hoje é: ${data} </p>;
-  saida.innerHTML += <h2>${diasDaSemana[diaHoje]}</h2>;
-  saida.innerHTML += <p>Amanhã será ${diasDaSemana[amanha]}</p>;
-  saida.innerHTML += <p>Ontem foi ${diasDaSemana[ontem]}</p>;
-}
+let hoje = new Date;
+let diasDaSemana = new Array("Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado");
+let diaHoje = hoje.getDay();
+let ontem = diaHoje - 1;
+let amanha = diaHoje + 1;
+let data = hoje.getDate() + "/" + hoje.getMonth() + 1 + "/" + hoje.getFullYear();
+saida.innerHTML = `<p>Hoje é: ${data}</p>`
+saida.innerHTML += `<h2>${diasDaSemana[diaHoje]}</h3>`;
+saida.innerHTML += `<p>Amanhã será: ${diasDaSemana[amanha]}</p>`;
+saida.innerHTML += `<p>Ontem foi: ${diasDaSemana[ontem]}</p>`;
