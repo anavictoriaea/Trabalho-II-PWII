4e0d74c6aae851243e9713195d3b9b0141cc55ee
anavictoriaea <avea@aluno.ifnmg.edu.br>
2022-07-21 08:47:10 -0300

    1 file modified, 3 added, 2 deleted

---
+++ atividades/atv1.js
@@ -0,0 +1,12 @@
+function media(){
+  let nome = document.m.nome.value;
+  let s1 = parseFloat(document.m.s1.value);
+  let s2 = parseFloat(document.m.s2.value);
+
+  if (s1 <= 0.0 | s2 <= 0.0){
+    alert("Digite os dados corretamente.")
+  } else {
+    let med = (s1 + s2) / 2;
+    document.getElementById("mostrar").textContent = "Funcionário: " + nome + ". Média: $" + med;
+  }
+}
